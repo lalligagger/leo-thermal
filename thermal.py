381db75aeb7aa0f5b84dd00b77404ebc551f701d
Kevin Lalli <klalli@hydrosat.com>
2021-12-27 18:10:00 -0800

    first commit

---
+++ thermal.py
@@ -0,0 +1,150 @@
+import xsimlab as xs
+import numpy as np
+
+boltz = 5.67 * 1e-8  # W/m^2/K^4
+
+@xs.process
+class Spacecraft:
+    A_inc = xs.variable(
+        intent="in", description="space facing area", default=1.0, groups="sc_vars"
+    )
+    A_rad = xs.variable(intent="in", description="earth facing area", default=1.0)
+    absorb = xs.variable(
+        intent="in",
+        description="spacecraft absorptivity",
+        default=0.96,
+        groups="sc_vars",
+    )
+    emis = xs.variable(
+        intent="in", description="spacecraft emissivity", default=0.9, groups="sc_vars"
+    )
+    mass = xs.variable(
+        intent="in", description="spacecraft mass", default=12.0
+    )  # kg
+    spec_heat = xs.variable(
+        intent="in", description="spacecraft specific heat", default=897.0
+    )  # J/(kg*K)
+    Q_gen = xs.variable(
+        intent="in",
+        description="spacecraft power dissipation",
+        default=15.0,
+        groups="sc_vars",
+    )  # @
+
+    heat_capacity = xs.variable(
+        intent="out", description="spacecraft heat capacity", groups="sc_vars"
+    )  # J/K
+
+    def initialize(self):
+        self.heat_capacity = self.spec_heat * self.mass
+
+
+@xs.process
+class Orbit:
+    R = xs.variable(intent="in", description="body radius", default=6.3781e6)  # meters
+    h = xs.variable(intent="in", description="orbit altitude", default=525e3)  # meters
+    tau = xs.variable(
+        intent="in", description="orbit period", default=90 * 60
+    )  # orbital period, seconds
+    case = xs.variable(intent="in", description="hot/ cold case", default="hot")
+    beta = xs.variable(intent="in", description="beta, radians", default=0)
+
+    f_E = xs.variable(intent="out", description="eclipse fraction")
+    q_sol = xs.variable(intent="out", description="solar loading")
+    albedo = xs.variable(intent="out", description="earth albedo")
+    q_ir = xs.variable(intent="out", description="earth IR")
+
+    def initialize(self):
+        # Solar radiation at parhelion and aphelion
+        q_sol = {"hot": 1414, "cold": 1322}  # W/m^2
+        self.q_sol = q_sol[self.case]
+
+        beta_star = np.arcsin(self.R / (self.R + self.h))  # beta angle, radians
+        if self.beta >= beta_star:
+            self.f_E = 0
+        else:
+            self.f_E = (
+                np.arccos(
+                    np.sqrt(self.h ** 2 + 2 * self.R * self.h)
+                    / ((self.R + self.h) * np.cos(self.beta))
+                )
+                / np.pi
+            )
+
+        if self.beta >= 30:
+            self.albedo = 0.19
+            self.q_ir = 218
+        else:
+            self.albedo = 0.14
+            self.q_ir = 228
+
+
+@xs.process
+class SingleNode:
+    """Wrap single-mode thermal model in a single Process."""
+
+    T_init = xs.variable(intent="in", description="initial temperature", default=290.0)
+    T_out = xs.variable(intent="out", description="model temperature")
+
+    tau = xs.foreign(Orbit, "tau")
+    q_sol = xs.foreign(Orbit, "q_sol", intent="in")
+    f_E = xs.foreign(Orbit, "f_E", intent="in")
+    q_ir = xs.foreign(Orbit, "q_ir", intent="in")
+    albedo = xs.foreign(Orbit, "albedo", intent="in")
+
+    sc_vars = xs.group_dict("sc_vars")
+
+    def initialize(self):
+        self.T_out = self.T_init
+        self.time = 0
+        # self.A_inc = self.sc_vars[("spacecraft", "A_inc")]
+
+    @xs.runtime(args="step_delta")
+    def run_step(self, dt):
+
+        # eclipse function
+        if (self.time % self.tau) / self.tau >= self.f_E:
+            sol_rad = 1
+        else:
+            sol_rad = 0
+
+        # heat from incident radiation from Earth IR
+        # Q1 = self.q_ir * self.A_inc
+        Q1 = self.q_ir * self.sc_vars[("spacecraft", "A_inc")]
+
+        # heat from incident radiation from Sun + Earthshine
+        # Q2 = (1 + self.albedo) * self.q_sol * self.A_inc * sol_rad * self.sc_absorb
+        Q2 = (
+            (1 + self.albedo)
+            * self.q_sol
+            * self.sc_vars[("spacecraft", "A_inc")]
+            * sol_rad
+            * self.sc_vars[("spacecraft", "absorb")]
+        )
+
+        # internally generated heat
+        Q3 = self.sc_vars[("spacecraft", "Q_gen")]
+
+        # heat from emited area to space
+        Q4 = (
+            self.sc_vars[("spacecraft", "A_inc")]
+            * boltz
+            * self.sc_vars[("spacecraft", "emis")]
+            * self.T_out ** 4
+        )  # Modified from paper to A_inc for simple radiaitor
+
+        # total heat flow
+        Q_dot = Q1 + Q2 + Q3 - Q4
+
+        dTdt = Q_dot / (self.sc_vars[("spacecraft", "heat_capacity")])
+
+        self.T1 = self.T_out + dTdt * dt
+        self.time += dt
+
+    def finalize_step(self):
+        self.T_out = self.T1
+
+
+single_node = xs.Model(
+    {"spacecraft": Spacecraft, "orbit": Orbit, "thermal": SingleNode}
+)
